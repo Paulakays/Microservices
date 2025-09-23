85695838a0b7076061a3c9793dc34879a1247a05
Paulakays <anythingpaula@gmail.com>
2025-09-23 17:37:04 +0200

    Added Docker configuration

---
+++ OnlineRegistrationSystem/registration-service/src/main/java/com/studiosus/trainee/registrationservice/StudentClient.java
@@ -0,0 +1,23 @@
+package com.studiosus.trainee.registrationservice;
+
+import com.studiosus.trainee.registrationservice.StudentDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class StudentClient {
+
+	private final RestTemplate restTemplate;
+	private final String studentServiceUrl;
+
+	public StudentClient(RestTemplate restTemplate, @Value("${student.service.url}") String studentServiceUrl) {
+		this.restTemplate = restTemplate;
+		this.studentServiceUrl = studentServiceUrl;
+	}
+
+	public StudentDTO getStudentById(int studentId) {
+		String url = studentServiceUrl + "/students/" + studentId;
+		return restTemplate.getForObject(url, StudentDTO.class);
+	}
+}
